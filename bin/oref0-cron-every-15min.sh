63355f134723b08063abd699b37e8a37e04bc95e
Łukasz Arczyński <larczynski@opera.com>
2021-03-16 13:24:27 +0100

    Revert "Add a shutdown function for pi based rigs (#1331)"
    This reverts commit 303833cf9952fe1e4777f18ffb27d267dfcf7627.

--- bin/oref0-cron-every-15min.sh
+++ bin/oref0-cron-every-15min.sh
@@ -21,17 +21,6 @@ if is_edison; then
     fi
 fi
 
-# proper shutdown of pi rigs once the battery level is below 2 % (should be more than enough to shut down on a standard 18600 ~2Ah cell)
-if is_pi; then
-    BATTERY_PERCENT="$(sudo ~/src/openaps-menu/scripts/getvoltage.sh | tee ~/myopenaps/monitor/edison-battery.json | jq .battery)"
-    BATTERY_CUTOFF=$(get_pref_float .pi_battery_shutdown_percent 2)
-    echo "Battery level is $BATTERY_PERCENT percent"
-    if (( "$BATTERY_PERCENT" < "$BATTERY_CUTOFF" )); then
-        echo "Critically low battery! Shutting down."
-        sudo shutdown -h now
-    fi
-fi
-
 # temporarily disable hotspot for 1m every 15m to allow it to try to connect via wifi again
 (
     touch /tmp/disable_hotspot
