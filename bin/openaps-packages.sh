94728372f337d975fb63d67a7922015f0e62ebf1
Łukasz Arczyński <larczynski@opera.com>
2025-11-04 16:41:15 +0100

    node fix

--- bin/openaps-packages.sh
+++ bin/openaps-packages.sh
@@ -44,18 +44,18 @@ else
    sudo apt-get -y install jq || die "Couldn't install jq"
 fi
 
-# Install node using n if there is not an installed version of node >=8,<=19
+# Install node using n if there is not an installed version of node >=8,<=18
 # Edge case: This is not likely to work as expected if there *is* a version of node installed, but it is outside of the specified version constraints
-if ! node --version | grep -q -e 'v[89]\.' -e 'v1[[:digit:]]\.'; then
+if ! node --version 2>/dev/null | grep -q -e 'v[89]\.' -e 'v1[0-8]\.'; then
    echo "Installing node via n..." # For context why we don't install using apt or nvm, see https://github.com/openaps/oref0/pull/1419
    curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
-   # Install the latest compatible version of node
-   sudo bash n current
+   # Install Node 18 LTS (last version in the acceptable range)
+   sudo bash n lts
    # Delete the local n binary used to boostrap the install
    rm n
    # Install n globally
    sudo npm install -g n
-   
+
    # Upgrade to the latest supported version of npm for the current node version
    sudo npm upgrade -g npm|| die "Couldn't update npm"
 
